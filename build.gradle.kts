dd4e39ec238e4fd3bb09ca5b2ea284fdbf1c04a8
kpavlov <{ID}+{username}@users.noreply.github.com>
2025-02-25 10:16:48 +0200

    cleanup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     base
-//    `maven-publish`
     alias(libs.plugins.detekt)
     alias(libs.plugins.dokka)
     alias(libs.plugins.dokka.javadoc)
@@ -19,9 +18,6 @@ allprojects {
 
 // Common configuration for subprojects
 subprojects {
-//    apply(plugin = "org.jetbrains.kotlin.multiplatform")
-//    apply(plugin = "maven-publish")
-//    apply(plugin = "signing")
     apply(plugin = "org.jetbrains.dokka")
     apply(plugin = "org.jetbrains.dokka-javadoc")
     apply(plugin = "com.diffplug.spotless")
@@ -31,52 +27,6 @@ subprojects {
         from("build/dokka/javadoc")
         archiveClassifier.set("javadoc")
     }
-
-//    publishing {
-//        publications {
-//            create<MavenPublication>("maven") {
-//                from(components["kotlin"])
-//                artifact(tasks["dokkaJavadocJar"])
-//
-//                // Use project-level name
-//                artifactId = project.name
-//
-//                pom {
-//                    name = project.name
-//                    description = project.description
-//                    url = "https://github.com/kpavlov/ai-mocks"
-//                    licenses {
-//                        license {
-//                            name = "MIT License"
-//                            url = "https://opensource.org/licenses/MIT"
-//                        }
-//                    }
-//                    developers {
-//                        developer {
-//                            id = "kpavlov"
-//                            name = "Konstantin Pavlov"
-//                            url = "https://github.com/kpavlov"
-//                        }
-//                    }
-//                    scm {
-//                        connection = "scm:git:git://github.com/kpavlov/ai-mocks.git"
-//                        developerConnection = "scm:git:ssh://github.com/kpavlov/ai-mocks.git"
-//                        url = "https://github.com/kpavlov/ai-mocks"
-//                    }
-//                }
-//            }
-//        }
-//    }
-
-//    signing {
-//        sign(publishing.publications["maven"])
-//    }
-
-//    spotless {
-//        kotlin {
-//            ktlint()
-//        }
-//    }
 }
 
 dependencies {
