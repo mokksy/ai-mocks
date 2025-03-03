331afc50a8c4775ecb3dbec7f5f59de2e3f7d740
Konstantin Pavlov <1517853+kpavlov@users.noreply.github.com>
2025-03-03 10:54:31 +0200

    Add Jackson support and improve content negotiation configuration (#63)
    This commit introduces support for Jackson serialization/deserialization
    in Mokksy. It refactors `ServerConfiguration` to allow customizable
    content negotiation via a functional parameter. Additionally, a new
    integration test validates the Jackson serialization setup.

--- mokksy/src/jvmMain/kotlin/me/kpavlov/mokksy/MokksyServer.jvm.kt
+++ mokksy/src/jvmMain/kotlin/me/kpavlov/mokksy/MokksyServer.jvm.kt
@@ -1,5 +1,6 @@
 package me.kpavlov.mokksy
 
+import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.engine.ApplicationEngine
@@ -7,6 +8,9 @@ import io.ktor.server.engine.EmbeddedServer
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import io.ktor.server.plugins.calllogging.CallLogging
+import io.ktor.server.plugins.contentnegotiation.ContentNegotiationConfig
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
 import org.slf4j.event.Level
 
 internal actual fun createEmbeddedServer(
@@ -25,6 +29,15 @@ internal actual fun createEmbeddedServer(
     ) {
         module()
         install(CallLogging) {
-            this.level = if (configuration.verbose) Level.DEBUG else Level.INFO
+            level = if (configuration.verbose) Level.DEBUG else Level.INFO
         }
     }
+
+@OptIn(ExperimentalSerializationApi::class)
+internal actual fun configureContentNegotiation(config: ContentNegotiationConfig) {
+    config.json(
+        Json {
+            ignoreUnknownKeys = true
+        },
+    )
+}
