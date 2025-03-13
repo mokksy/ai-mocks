56e9bca681314524299281e719f865ffe51f4f9d
kpavlov <1517853+kpavlov@users.noreply.github.com>
2025-03-13 15:24:30 +0200

    update test

--- ai-mocks-anthropic/src/jvmTest/kotlin/me/kpavlov/aimocks/anthropic/official/MessagesTest.kt
+++ ai-mocks-anthropic/src/jvmTest/kotlin/me/kpavlov/aimocks/anthropic/official/MessagesTest.kt
@@ -3,6 +3,7 @@ package me.kpavlov.aimocks.anthropic.official
 import com.anthropic.client.AnthropicClient
 import com.anthropic.client.okhttp.AnthropicOkHttpClient
 import com.anthropic.models.MessageCreateParams
+import com.anthropic.models.Metadata
 import me.kpavlov.aimocks.anthropic.anthropic
 import kotlin.test.Test
 import kotlin.time.Duration.Companion.milliseconds
@@ -22,7 +23,8 @@ internal class MessagesTest : AbstractAnthropicTest() {
             temperature = 0.42
             model = "claude-3-7-sonnet-latest"
             maxCompletionTokens = 100
-            systemMessageContains("helpful assistant")
+            userId = userIdValue
+            systemMessageContains("helpful assistant #$userIdValue")
             userMessageContains("say 'Hello!'")
         } responds {
             assistantContent = "Hello"
@@ -34,7 +36,8 @@ internal class MessagesTest : AbstractAnthropicTest() {
                 .builder()
                 .temperature(0.42)
                 .maxTokens(100)
-                .system("You are a helpful assistant.")
+                .metadata(Metadata.builder().userId(userIdValue).build())
+                .system("You are a helpful assistant #$userIdValue")
                 .addUserMessage("Just say 'Hello!' and nothing else")
                 .model("claude-3-7-sonnet-latest")
                 .build()
