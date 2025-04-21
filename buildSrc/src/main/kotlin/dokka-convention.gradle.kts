18349a08855ea8604f9c4fa54528f3fdf6bdf3ee
kpavlov <1517853+kpavlov@users.noreply.github.com>
2025-04-21 16:08:19 +0300

    dokka

--- buildSrc/src/main/kotlin/dokka-convention.gradle.kts
+++ buildSrc/src/main/kotlin/dokka-convention.gradle.kts
@@ -1,9 +1,27 @@
+import org.jetbrains.dokka.gradle.DokkaTask
+
 /**
  * Common conventions for generating documentation with Dokka.
  */
 
 plugins {
     id("org.jetbrains.dokka")
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    dokkaSourceSets.configureEach {
+        externalDocumentationLink {
+            url.set(uri("https://kotlinlang.org/api/kotlinx.serialization/").toURL())
+            packageListUrl.set(
+                rootProject.projectDir.resolve("serialization.package.list").toURI().toURL()
+            )
+        }
+        sourceRoots.from(
+            file("src/commonMain/kotlin"),
+            file("src/jvmMain/kotlin")
+        )
+    }
+
 }
 
 dokka {
