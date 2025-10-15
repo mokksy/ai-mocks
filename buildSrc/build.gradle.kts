ebf2faeddbff243eea3ed5a0377ce65f4ae63611
Konstantin Pavlov <1517853+kpavlov@users.noreply.github.com>
2025-10-15 14:38:46 +0300

    Upgrade dokka

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,6 @@ dependencies {
     implementation("com.diffplug.spotless:spotless-plugin-gradle:8.0.0")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.21")
     implementation("com.vanniktech:gradle-maven-publish-plugin:0.34.0")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:2.0.0")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:2.1.0")
     implementation("com.gradleup.shadow:shadow-gradle-plugin:9.2.2")
 }
