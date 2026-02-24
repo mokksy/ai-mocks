8e24d5de48350227eab2c1a700397d62dfe4620b
kpavlov <1517853+kpavlov@users.noreply.github.com>
2026-02-24 10:09:26 +0200

    test: refactor RequestSpecificationTest
    Simplified `RequestSpecificationTest` by eliminating `@MockK` annotations and nested class structures. Refactored to use direct mocking within test methods to enhance readability and streamline the test code.

--- mokksy/src/jvmTest/kotlin/dev/mokksy/mokksy/RequestSpecificationTest.kt
+++ mokksy/src/jvmTest/kotlin/dev/mokksy/mokksy/RequestSpecificationTest.kt
@@ -14,157 +14,147 @@ import io.ktor.server.request.receive
 import io.ktor.util.reflect.TypeInfo
 import io.mockk.coEvery
 import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
 import kotlinx.serialization.json.Json
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.jupiter.api.parallel.Execution
-import org.junit.jupiter.api.parallel.ExecutionMode
 import kotlin.test.Test
 
-@ExtendWith(MockKExtension::class)
-@Execution(ExecutionMode.SAME_THREAD)
 class RequestSpecificationTest {
-    @MockK
-    lateinit var request: ApplicationRequest
+    @Test
+    fun `should match when all conditions are satisfied`() =
+        runTest {
+            val request = mockk<ApplicationRequest>()
+            val input = Input("Alice")
 
-    @Nested
-    inner class SuccessfulMatches {
-        @Test
-        fun `should match when all conditions are satisfied`() =
-            runTest {
-                val input = Input("Alice")
+            every { request.httpMethod } returns HttpMethod.Get
+            every { request.path() } returns "/test"
+            coEvery<Input?> { request.call.receiveNullable(any<TypeInfo>()) } returns input
+            coEvery { request.call.receive(String::class) } returns Json.encodeToString(input)
+            every { request.headers } returns
+                Headers.build {
+                    append("X-Request-ID", "RequestID")
+                }
+            val headersMatcher = mockk<Matcher<Headers>>(relaxed = true)
+            every { headersMatcher.test(any()).passed() } returns true
 
-                every { request.httpMethod } returns HttpMethod.Get
-                every { request.path() } returns "/test"
-                coEvery<Input?> { request.call.receiveNullable(any<TypeInfo>()) } returns input
-                coEvery { request.call.receive(String::class) } returns Json.encodeToString(input)
-                coEvery { request.headers } returns
-                    Headers.build {
-                        append("X-Request-ID", "RequestID")
-                    }
-                val headersMatcher = mockk<Matcher<Headers>>(relaxed = true)
-                every { headersMatcher.test(any()).passed() } returns true
+            val spec =
+                RequestSpecification(
+                    method = beEqual(HttpMethod.Get),
+                    path = contain("test"),
+                    headers = listOf(headersMatcher),
+                    body = listOf(beEqual(Input("Alice"))),
+                    bodyString = listOf(contain("Alice")),
+                    requestType = Input::class,
+                )
 
-                val spec =
-                    RequestSpecification(
-                        method = beEqual(HttpMethod.Get),
-                        path = contain("test"),
-                        headers = listOf(headersMatcher),
-                        body = listOf(beEqual(Input("Alice"))),
-                        bodyString = listOf(contain("Alice")),
-                        requestType = Input::class,
-                    )
+            spec.matches(request) shouldBe Result.success(true)
+        }
 
-                spec.matches(request) shouldBe Result.success(true)
-            }
+    @Test
+    fun `should match when only headers are specified`() =
+        runTest {
+            val request = mockk<ApplicationRequest>()
+            every { request.headers } returns
+                Headers.build {
+                    append("X-Request-ID", "RequestID")
+                }
 
-        @Test
-        fun `should match when only headers are specified`() =
-            runTest {
-                coEvery { request.headers } returns
-                    Headers.build {
-                        append("X-Request-ID", "RequestID")
-                    }
+            val headersMatcher = mockk<Matcher<Headers>>(relaxed = true)
+            val spec =
+                RequestSpecification(
+                    headers = listOf(headersMatcher),
+                    requestType = Input::class,
+                )
 
-                val headersMatcher = mockk<Matcher<Headers>>(relaxed = true)
-                val spec =
-                    RequestSpecification(
-                        headers = listOf(headersMatcher),
-                        requestType = Input::class,
-                    )
+            every { headersMatcher.test(any()).passed() } returns true
 
-                every { headersMatcher.test(any()).passed() } returns true
+            spec.matches(request) shouldBe Result.success(true)
+        }
 
-                spec.matches(request) shouldBe Result.success(true)
-            }
-    }
+    @Test
+    fun mismatchedMethod() =
+        runTest {
+            val request = mockk<ApplicationRequest>()
+            every { request.httpMethod } returns HttpMethod.Get
+            val spec =
+                RequestSpecification(
+                    method = beEqual(HttpMethod.Post),
+                    path = contain("test"),
+                    requestType = Input::class,
+                )
 
-    @Nested
-    inner class FailedMatches {
-        @Test
-        fun mismatchedMethod() =
-            runTest {
-                every { request.httpMethod } returns HttpMethod.Get
-                val spec =
-                    RequestSpecification(
-                        method = beEqual(HttpMethod.Post),
-                        path = contain("test"),
-                        requestType = Input::class,
-                    )
+            spec.matches(request) shouldBe Result.success(false)
+        }
 
-                spec.matches(request) shouldBe Result.success(false)
-            }
+    @Test
+    fun mismatchedPath() =
+        runTest {
+            val request = mockk<ApplicationRequest>()
+            every { request.httpMethod } returns HttpMethod.Get
+            every { request.path() } returns "/other"
 
-        @Test
-        fun mismatchedPath() =
-            runTest {
-                every { request.httpMethod } returns HttpMethod.Get
-                every { request.path() } returns "/test"
+            val spec =
+                RequestSpecification(
+                    method = beEqual(HttpMethod.Get),
+                    path = contain("test"),
+                    requestType = Input::class,
+                )
 
-                val spec =
-                    RequestSpecification(
-                        method = beEqual(HttpMethod.Get),
-                        path = contain("differentPath"),
-                        requestType = Input::class,
-                    )
+            spec.matches(request) shouldBe Result.success(false)
+        }
 
-                spec.matches(request) shouldBe Result.success(false)
-            }
+    @Test
+    fun mismatchedHeaders() =
+        runTest {
+            val request = mockk<ApplicationRequest>()
+            every { request.headers } returns
+                Headers.build {
+                    append("X-Request-ID", "RequestID")
+                }
 
-        @Test
-        fun mismatchedHeaders() =
-            runTest {
-                coEvery { request.headers } returns
-                    Headers.build {
-                        append("X-Request-ID", "RequestID")
-                    }
+            val headersMatcher = mockk<Matcher<Headers>>(relaxed = true)
+            val spec =
+                RequestSpecification(
+                    headers = listOf(headersMatcher),
+                    requestType = Input::class,
+                )
 
-                val headersMatcher = mockk<Matcher<Headers>>(relaxed = true)
-                val spec =
-                    RequestSpecification(
-                        headers = listOf(headersMatcher),
-                        requestType = Input::class,
-                    )
+            every { headersMatcher.test(any()).passed() } returns false
 
-                every { headersMatcher.test(any()).passed() } returns false
+            spec.matches(request) shouldBe Result.success(false)
+        }
 
-                spec.matches(request) shouldBe Result.success(false)
-            }
+    @Test
+    fun `should not match when bodyString differs`() =
+        runTest {
+            val request = mockk<ApplicationRequest>()
+            coEvery { request.call.receive(String::class) } returns "Another body"
 
-        @Test
-        fun `should not match when bodyString differs`() =
-            runTest {
-                coEvery { request.call.receive(String::class) } returns "Another body"
+            val bodyMatcher = contain("expectedBody")
+            val spec =
+                RequestSpecification(
+                    bodyString = listOf(bodyMatcher),
+                    requestType = String::class,
+                )
 
-                val bodyMatcher = contain("expectedBody")
-                val spec =
-                    RequestSpecification(
-                        bodyString = listOf(bodyMatcher),
-                        requestType = String::class,
-                    )
+            spec.matches(request) shouldBe Result.success(false)
+        }
 
-                spec.matches(request) shouldBe Result.success(false)
-            }
+    @Test
+    fun `should not match when body differs`() =
+        runTest {
+            val request = mockk<ApplicationRequest>()
+            val input = Input("Alice")
 
-        @Test
-        fun `should not match when body differs`() =
-            runTest {
-                val input = Input("Alice")
+            coEvery<Input?> { request.call.receiveNullable(any<TypeInfo>()) } returns input
 
-                coEvery<Input?> { request.call.receiveNullable(any<TypeInfo>()) } returns input
+            val bodyMatcher: Matcher<Input?> = beEqual(Input("Bob"))
+            val spec =
+                RequestSpecification(
+                    body = listOf(bodyMatcher),
+                    requestType = Input::class,
+                )
 
-                val bodyMatcher: Matcher<Input?> = beEqual(Input("Bob"))
-                val spec =
-                    RequestSpecification(
-                        body = listOf(bodyMatcher),
-                        requestType = Input::class,
-                    )
-
-                spec.matches(request) shouldBe Result.success(false)
-            }
-    }
+            spec.matches(request) shouldBe Result.success(false)
+        }
 }
